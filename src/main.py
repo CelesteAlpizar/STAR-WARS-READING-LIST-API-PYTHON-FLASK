08d11fe8360f5f6fda593d1d932586adb790aaef
María Celeste Alpízar Solano <maria.alpizar.72@gmail.com>
2021-04-08 23:33:39 +0000

    In process

--- src/main.py
+++ src/main.py
@@ -2,13 +2,14 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 import os
+import json
 from flask import Flask, request, jsonify, url_for
 from flask_migrate import Migrate
 from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Character, Planet
 #from models import Person
 
 app = Flask(__name__)
@@ -31,13 +32,37 @@ def sitemap():
     return generate_sitemap(app)
 
 @app.route('/user', methods=['GET'])
-def handle_hello():
+def handle_allUsers():
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+    return jsonify(User.uservalues()), 200
 
-    return jsonify(response_body), 200
+@app.route('/user', methods=['POST'])
+def handle_addUsers():
+    request_body_user=request.data
+    decoded_object = json.loads(request_body_user)
+    return jsonify(User.add_user(decoded_object)), 200
+
+@app.route('/characters', methods=['GET'])
+def handle_allCharacters():
+
+    return jsonify(Character.charactersvalues()), 200
+
+@app.route('/characters', methods=['POST'])
+def handle_addCharacters():
+    request_body_character=request.data
+    decoded_object = json.loads(request_body_character)
+    return jsonify(Character.add_character(decoded_object)), 200
+
+@app.route('/planets', methods=['GET'])
+def handle_allPlanets():
+
+    return jsonify(Planet.planetsvalues()), 200
+
+@app.route('/planets', methods=['POST'])
+def handle_addPlanets():
+    request_body_planet=request.data
+    decoded_object = json.loads(request_body_planet)
+    return jsonify(Planet.add_planet(decoded_object)), 200
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -4,16 +4,91 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(120), unique=True, nullable=False)
+    fullname = db.Column(db.String(120), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    is_active = db.Column(db.Boolean(), unique=False, nullable=False, default=True)
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<User %r>' % self.username 
+        # no es necesaria tenerla pero lo que esta funcion hace es permitir que se lea
 
     def serialize(self):
         return {
             "id": self.id,
+            "username": self.username,
+            "fullname": self.fullname,
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+    def uservalues():
+        user_query = User.query.all()
+        all_user = list(map(lambda x: x.serialize(), user_query))
+        return all_user
+
+    def add_user(request_body_user):
+        user = User(fullname=request_body_user["fullname"], username=request_body_user["username"], email=request_body_user["email"], password=request_body_user["password"])
+        db.session.add(user)
+        db.session.commit()
+        return("An user has been added")
+
+class Character(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    birth_year = db.Column(db.String(120), unique=True, nullable=False)
+    gender = db.Column(db.String(120), unique=True, nullable=False)
+    height = db.Column(db.String(120), unique=False, nullable=False)
+    skin_color = db.Column(db.String(120), unique=True, nullable=False)
+    eye_color = db.Column(db.String(120), unique=False, nullable=False)
+
+    def __repr__(self):
+        return '<Character %r>' % self.name 
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+        }
+
+    def charactersvalues():
+        characters_query = Character.query.all()
+        all_characters = list(map(lambda x: x.serialize(), characters_query))
+        return all_characters
+
+    def add_character(request_body_character):
+        character = Character(name=request_body_character["name"], birth_year=request_body_character["birth_year"], gender=request_body_character["gender"], height=request_body_character["height"], skin_color=request_body_character["skin_color"], eye_color=request_body_character["eye_color"])
+        db.session.add(character)
+        db.session.commit()
+        return("A character has been added")
+
+
+class Planet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    climate = db.Column(db.String(120), unique=True, nullable=False)
+    population = db.Column(db.String(120), unique=True, nullable=False)
+    orbital_period = db.Column(db.String(120), unique=False, nullable=False)
+    rotation_period = db.Column(db.String(120), unique=True, nullable=False)
+    diameter = db.Column(db.String(120), unique=False, nullable=False)
+
+    def __repr__(self):
+        return '<Planet %r>' % self.name 
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+        }
+
+    def planetsvalues():
+        planets_query = Planet.query.all()
+        all_planets = list(map(lambda x: x.serialize(), planets_query))
+        return all_planets
+
+    def add_planet(request_body_planet):
+        planet = Planet(name=request_body_planet["name"], climate=request_body_planet["climate"], population=request_body_planet["population"], orbital_period=request_body_planet["orbital_period"], rotation_period=request_body_planet["rotation_period"], diameter=request_body_planet["diameter"])
+        db.session.add(planet)
+        db.session.commit()
+        return("A planet has been added")
